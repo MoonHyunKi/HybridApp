83902a60025aa145de1c779193d1c8b17891f3a6
MoonHyunKi <lhk1130200@naver.com>
2022-12-06 11:38:00 +0900

    블록 생성 및 합치기

--- 2048/2048.js
+++ 2048/2048.js
@@ -2,6 +2,7 @@ var board;
 var score = 0;
 var rows = 4;
 var columns = 4;
+var ran = 3;
 
 window.onload = function() {
     setGame();
@@ -14,7 +15,7 @@ function setGame() {
     //     [4, 4, 8, 8],
     //     [4, 4, 8, 8]
     // ];
-
+    
     board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -32,8 +33,56 @@ function setGame() {
         }
     }
     //create 2 to begin the game
-    setTwo();
-    setTwo();
+    
+    // let ranNum = Math.floor(Math.random() * ran * 10);
+    //             if(ranNum == 1){
+    //                 setOne();
+    //             } else if(ranNum == 2){
+    //                 setOne();
+    //                 setThree();
+    //             } else if(ranNum == 3){
+    //                 setThree();
+    //             }
+    function random() {
+        return Math.floor(Math.random() * 10)}
+      
+        const a = random()   
+        switch (a) {
+        case 0:
+            setOne();
+          break
+          case 1:
+            setOne();
+          break
+          case 2:
+            setOne();
+          break
+          case 3:
+            setOne();
+          break
+        case 4:
+            setOne();
+            setThree();
+          break
+          case 5:
+            setOne();
+            setThree();
+          break
+          case 6:
+            setOne();
+            setThree();
+          break
+            case 7:
+            setThree();
+          break
+          case 8:
+            setThree();
+          break
+          case 9:
+            setThree();
+          break
+        default:
+    }
 }
 
 function updateTile(tile, num) {
@@ -50,23 +99,24 @@ function updateTile(tile, num) {
     }
 }
 
+
 document.addEventListener('keyup', (e) => {
     if (e.code == "ArrowLeft") {
         slideLeft();
-        setTwo();
+        setOne();
     }
     else if (e.code == "ArrowRight") {
         slideRight();
-        setTwo();
+        setOne();
     }
     else if (e.code == "ArrowUp") {
         slideUp();
-        setTwo();
+        setOne();
 
     }
     else if (e.code == "ArrowDown") {
         slideDown();
-        setTwo();
+        setOne();
     }
     document.getElementById("score").innerText = score;
 })
@@ -80,9 +130,9 @@ function slide(row) {
     row = filterZero(row); //[2, 2, 2]
     for (let i = 0; i < row.length-1; i++){
         if (row[i] == row[i+1]) {
-            //row[i] *= 2;
-            //row[i+1] = 0;
-            //score += row[i];
+            row[i] *= 2;
+            row[i+1] = 0;
+            score += row[i];
         }
     } //[4, 0, 2]
     row = filterZero(row); //[4, 2]
@@ -166,12 +216,60 @@ function setTwo() {
         let r = Math.floor(Math.random() * rows);
         let c = Math.floor(Math.random() * columns);
         if (board[r][c] == 0) {
-            board[r][c] = 2;
-            let tile = document.getElementById(r.toString() + "-" + c.toString());
-            tile.innerText = "2";
-            tile.classList.add("x2");
-            found = true;
-        }
+            let ranNum = Math.floor(Math.random() * ran * 10);
+                if(ranNum == 1){
+                    board[r][c] = 2;
+                    let tile = document.getElementById(r.toString() + "-" + c.toString());
+                    tile.innerText = "2";
+                    tile.classList.add("x2");
+                    found = true;
+
+                } else if(ranNum == 2){
+                    board[r][c] = 4;
+                    let tile = document.getElementById(r.toString() + "-" + c.toString());
+                    tile.innerText = "4";
+                    tile.classList.add("x4");
+                    found = true;
+                }
+            }
+    }
+}
+
+function setOne() {
+    if (!hasEmptyTile()) {
+        return;
+    }
+    let found = false;
+    while (!found) {
+        //find random row and column to place a 2 in
+        let ro = Math.floor(Math.random() * rows);
+        let co = Math.floor(Math.random() * columns);
+        if (board[ro][co] == 0) {
+                    board[ro][co] = 2;
+                    let tile = document.getElementById(ro.toString() + "-" + co.toString());
+                    tile.innerText = "2";
+                    tile.classList.add("x2");
+                    found = true;
+            }
+    }
+}
+
+function setThree() {
+    if (!hasEmptyTile()) {
+        return;
+    }
+    let found = false;
+    while (!found) {
+        //find random row and column to place a 2 in
+        let roo = Math.floor(Math.random() * rows);
+        let coo = Math.floor(Math.random() * columns);
+        if (board[roo][coo] == 0) {
+                    board[roo][coo] = 4;
+                    let tile = document.getElementById(roo.toString() + "-" + coo.toString());
+                    tile.innerText = "4";
+                    tile.classList.add("x4");
+                    found = true;
+            }
     }
 }
 
